fd8b5d922fefdfcd11f740309f9573e5f97955db
Vincent Smeets <Vincent.Smeets@atos.net>
2020-05-19 08:42:58 +0200

    Cleanup after 24h

--- cleanup/cleanup.sh
+++ cleanup/cleanup.sh
@@ -4,7 +4,7 @@
 # Delay random 1 to 5 minutes.
 sleep $((60 + ( $RANDOM % 240 ) ))
 
-docker system prune --force --filter until=168h --all
+docker system prune --force --filter until=24h --all
 
 volumeNames=`docker volume ls --filter 'dangling=true' --filter 'name=.{64}' --format '{{.Name}}'`
 [ -n "$volumeNames" ] \
